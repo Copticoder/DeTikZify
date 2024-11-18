b61362b4604179804dcbce3a41a8084480950bb5
Ahmed Emad Attia <90578831+Copticoder@users.noreply.github.com>
2024-11-18 21:33:01 +0400

    Update node.py

--- detikzify/mcts/node.py
+++ detikzify/mcts/node.py
@@ -22,37 +22,25 @@ class Node:
 
     def update_win_value(self, value):
         self.win_value += value
+        # DNG_MCTS parameters update   
         self.alpha_s += 0.5
-        self.beta_s += (self.lambda_s * (value - self.mu_s)**2 / (self.lambda_s + 1)) / 2
-        self.mu_s = (self.lambda_s * self.mu_s + value) / (self.lambda_s + 1)
+        self.beta_s += (self.lambda_s * (value.score - self.mu_s)**2 / (self.lambda_s + 1)) / 2
+        self.mu_s = (self.lambda_s * self.mu_s + value.score) / (self.lambda_s + 1)
         self.lambda_s += 1
-
+        ######
         self.visits += 1
 
         if self.parent:
             self.parent.update_win_value(value)
 
     def value_sampling(self, s_bar, sampling=True):
+        ### DNG-MCTS Value Sampling
         if sampling:
             tao = np.random.gamma(s_bar.alpha_s, 1 / s_bar.beta_s)
             std_dev = np.sqrt(1 / (tao * s_bar.lambda_s))
             mu = np.random.normal(s_bar.mu_s, std_dev)
             return mu
         return s_bar.mu_s
-    
-    def q_value(self, action, sampling= True):   
-        reward += self.value_sampling(self.children[s_bar], sampling)
-        return R_s_a+reward
-        
-    def best_child(self, action_space, **kwargs):
-        sampling = kwargs.get('sampling', True)
-        discount_factor = kwargs.get('discount_factor', 0.95)
-        action_values = []
-        for action in range(action_space):
-            q_value = self.q_value(action, discount_factor, sampling)
-            action_values.append(q_value)
-        selected_action = np.argmax(action_values)
-        return selected_action
     
     def update_policy_value(self, value):
         self.policy_value = value
@@ -65,12 +53,12 @@ class Node:
         for child in children:
             self.add_child(child)
 
-    def get_preferred_child(self, root_node):
+    def get_preferred_child(self):
         best_children = []
         best_score = float("-inf")
 
         for child in self.children:
-            score = child.get_score(root_node)
+            score = child.get_score()
 
             if score > best_score:
                 best_score = score
@@ -80,22 +68,8 @@ class Node:
 
         return random.choice(best_children)
 
-    def get_score(self, root_node):
-        discovery_operand = (
-            self.discovery_factor
-            * (self.policy_value or 1)
-            * np.sqrt(np.log(self.parent.visits) / (self.visits or 1))
-        )
-        
-        if self.is_widen_node:
-            win_operand = 0
-        else:
-            win_multiplier = (
-                1 if self.parent.player_number == root_node.player_number else -1
-            )
-            win_operand = win_multiplier * self.win_value / (self.visits or 1)
-
-        self.score = win_operand + discovery_operand
+    def get_score(self):
+        self.score = self.value_sampling(self, sampling=True)
 
         return self.score
 
